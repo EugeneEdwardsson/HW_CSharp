21b3e4d77034b728e7ae30048746cac12547fff6
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-03 12:07:41 +0300

    Task 3

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,33 +1,51 @@
 ﻿using System;
 
 
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+
+
+
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+
+
+
+
+
+
+
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.Write("Input the number of the day of the week: ");
-int inputNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input the number of the day of the week: ");
+// int inputNumber = Convert.ToInt32(Console.ReadLine());
 
-bool CheckDay(int number)
-{
-    if (number == 6 || number == 7)
-    {
-        Console.WriteLine("Six day!!! Satuday - day off");
-        return true;
-    } 
-    else if (number < 1 || number > 7)
-    {
-        Console.WriteLine("Incorrect entry in week 7 days!!!");
-        return false;
-    } 
-    else
-    {
-        Console.WriteLine("Week day!!! Work nigga! ");
-        return false;
-    }
+// bool CheckDay(int number)
+// {
+//     if (number == 6 || number == 7)
+//     {
+//         Console.WriteLine("Six day!!! Satuday - day off");
+//         return true;
+//     } 
+//     else if (number < 1 || number > 7)
+//     {
+//         Console.WriteLine("Incorrect entry in week 7 days!!!");
+//         return false;
+//     } 
+//     else
+//     {
+//         Console.WriteLine("Week day!!! Work nigga! ");
+//         return false;
+//     }
     
 
-}
+// }
 
 
-bool dayWeek = CheckDay(inputNumber);
-Console.WriteLine(dayWeek);
+// bool dayWeek = CheckDay(inputNumber);
+// Console.WriteLine(dayWeek);
 
