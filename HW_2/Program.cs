77067c7679c062dc2a6a134abd87b3e54b3159e8
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-03 15:23:39 +0300

    Task 2

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -3,26 +3,50 @@
 
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int numberRandom = new Random().Next(100,1000);
-Console.WriteLine($"Random three digit number: {numberRandom}");
-
-int ShowTwoDigit(int number)
-{
-int div = number / 10;
-
-return div % 10;
-
-}
-
-int twoDigit = ShowTwoDigit(numberRandom);
-Console.WriteLine($"Second digit number: {twoDigit}");
 
 
+// int numberRandom = new Random().Next(100,1000);
+// Console.WriteLine($"Random three digit number: {numberRandom}");
+
+// int ShowTwoDigit(int number)
+// {
+// int div = number / 10;
+
+// return div % 10;
+
+// }
+
+// int twoDigit = ShowTwoDigit(numberRandom);
+// Console.WriteLine($"Second digit number: {twoDigit}");
 
 
 
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Input the number: ");
+int givenNumber = Convert.ToInt32(Console.ReadLine());
+
+bool ShowThreeDigit(int number)
+{
+ string anyNumberString = Convert.ToString(number);
+ if (anyNumberString.Length > 2)
+ {
+    Console.WriteLine($"Three digit number: {anyNumberString[2]}");
+    return true;
+ }
+ else
+ {
+    Console.WriteLine("Entered number less than three digits: ");
+    return false;
+ }
+}
+
+
+Console.WriteLine(ShowThreeDigit(givenNumber));
+
+
 
 
 
