974ac82eaca319a43f45ec8ffe6912d5d4f66e7d
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-07 19:13:51 +0300

    HW 3, Task 2

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+        Console.Write("Введите координаты xA: ");
+        double firstX = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координаты yA: ");
+        double firstY = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координаты zA: ");
+        double firstZ = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координаты xB: ");
+        double secondX = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координаты yB: ");
+        double secondY = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координаты zB: ");
+        double secondZ = Convert.ToDouble(Console.ReadLine());
+
+
+    double FindDistance3D(double xA, double yA,  double zA, double xB, double yB, double zB)
+    {
+        double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
+        return Math.Round(distance, 2);
+    }
+
+
+    
+    double distance3D = FindDistance3D(firstX,secondX,firstY,secondY,firstZ,secondZ);
+
+    Console.WriteLine($"Distance between two points in 3D space: {distance3D}");
+
+
+
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
