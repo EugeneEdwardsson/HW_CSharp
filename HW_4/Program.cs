a193973b1c8681bafc0d394874ec44c6784afa13
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-10 20:01:48 +0300

    HW 4 Task 1

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.Write("Input number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponent(int numA, int numB){
+
+    int res = 1;
+
+    for (int i = 1; i <= numB; i++)
+    {
+        res = res * numA;
+    }
+
+    return res;
+
+}
+
+
+int exp = Exponent(numberA, numberB);
+Console.WriteLine($"{numberA} raised to the power {numberB} = {exp}");
+
+
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
