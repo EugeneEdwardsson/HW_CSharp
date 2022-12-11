5c6b401c74390e26d26ff257a7dfa476b67ca83f
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-11 14:53:05 +0300

    HW 4 Task 2

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -3,37 +3,53 @@
 //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 
-Console.Write("Input number A: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input number B: ");
-int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number A: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
 
-int Exponent(int numA, int numB){
+// int Exponent(int numA, int numB)
+// {
 
-    int res = 1;
+//     int res = 1;
 
-    for (int i = 1; i <= numB; i++)
-    {
-        res = res * numA;
-    }
+//     for (int i = 1; i <= numB; i++)
+//     {
+//         res = res * numA;
+//     }
 
-    return res;
+//     return res;
 
-}
+// }
 
 
-int exp = Exponent(numberA, numberB);
-Console.WriteLine($"{numberA} raised to the power {numberB} = {exp}");
-
+// int exp = Exponent(numberA, numberB);
+// Console.WriteLine($"{numberA} raised to the power {numberB} = {exp}");
 
 
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
 
+int sumDigitInNum(int num)
+{
+    int digit = 0;
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        digit = num % 10;
+        sum = sum + digit;
+        num = num / 10;
+    }
+    return sum;
+}
 
 
-
+int sumDigit = sumDigitInNum(number);
+Console.WriteLine($"Sum digit: {sumDigit}");
 
 
 
