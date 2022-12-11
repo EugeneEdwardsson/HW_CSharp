fd97b3dc74258c0c2dc0e299ba5be5b57872d647
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-11 20:47:55 +0300

    HW 4 Task 3

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -29,43 +29,63 @@
 
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-Console.Write("Input number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
 
-int sumDigitInNum(int num)
-{
-    int digit = 0;
-    int sum = 0;
+// int sumDigitInNum(int num)
+// {   
+//     int digit = 0;
+//     int sum = 0;
 
-    for (int i = 0; i < 10; i++)
-    {
-        digit = num % 10;
-        sum = sum + digit;
-        num = num / 10;
-    }
-    return sum;
-}
-
-
-int sumDigit = sumDigitInNum(number);
-Console.WriteLine($"Sum digit: {sumDigit}");
+//     for (int i = 0; i < 10; i++)
+//     {
+//         digit = num % 10;  
+//         num = num / 10; 
+//         sum = sum + digit;
+//     }
+//     return sum;
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// int sumDigit = sumDigitInNum(number);
+// Console.WriteLine($"Sum digit: {sumDigit}");
 
 
 
 //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+
+
+    for (int i = 0; i < size; i++)
+
+    {   
+        Console.Write("Input numbers of array: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{i + 1} element is array: {array[i]}");   
+    }
+}
+
+
+Console.Write("Input a number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[] myArray = CreateRandomArray(length);
+PrintArray(myArray);
