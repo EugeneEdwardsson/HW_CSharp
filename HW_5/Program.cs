802c6cb2b2773fa4e0e2959ee5a8cd8aaba554b6
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-15 20:29:04 +0300

    HW 5 Task 2

--- HW_5/Program.cs
+++ HW_5/Program.cs
@@ -55,53 +55,52 @@
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-    int[] array = new int[size];
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
 
-    for(int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return array;
-}
-
-
-void PrintArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }    
-    Console.WriteLine( );
-}
-
-int GetSumOfNegativePosition(int[] array)
-{
-    int sum = 0;
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(array[i] % 2 == 1)
-        {
-            sum ++;
-        }
-    }        
-    return sum;
-}
+//     for(int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
 
 
-Console.Write("Input number elements: ");
-int lenght = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a max possible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateRandomArray(lenght, min, max);
-PrintArray(myArray);
-int result = GetSumOfNegativePosition(myArray);
-Console.WriteLine($"Sum of positive elements is {result}");
+// void PrintArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }    
+//     Console.WriteLine( );
+// }
+
+// int GetSumOfNegativePosition(int[] array)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < array.Length; i = i + 2)
+//     {
+//         sum = sum + array[i];
+//     }        
+//     return sum;
+// }
+
+
+// Console.Write("Input number elements: ");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = CreateRandomArray(lenght, min, max);
+// PrintArray(myArray);
+// int result = GetSumOfNegativePosition(myArray);
+// Console.WriteLine($"Sum of elements in negative position is equal to {result}");
 
 
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+
