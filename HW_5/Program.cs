f4cc92b63e1c7862fd9a214f86a2b3d4009dc0c5
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-16 18:56:53 +0300

    HW5 Task 3 change method

--- HW_5/Program.cs
+++ HW_5/Program.cs
@@ -104,3 +104,65 @@
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 
+
+double[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+     double[] array = new double[size];
+     
+
+    for(int i = 0; i < size; i++)
+    {    
+        array[i] = Convert.ToInt32(new Random().Next(minValue,maxValue)) * 100 / 10; 
+    }    
+
+    for(int j = 1; j < size; j+=2)
+    {   
+        array[j] = new Random().NextDouble();
+       
+    }
+    
+    return array;
+
+
+}
+
+
+void PrintArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }    
+    Console.WriteLine();
+}
+
+void DiffMinMaxValues(double[] array)
+{
+      double minVal = array[0];
+      double maxVal = minVal;
+
+      for (int i = 0; i < array.Length; i++)
+      {
+         if (array[i] < minVal)
+         {
+             minVal = array[i];
+         }
+         if (array[i] > maxVal)
+         {
+             maxVal = array[i];
+         }
+      }
+    Console.WriteLine($"Difference between: {maxVal - minVal}");
+}
+
+
+  Console.Write("Input number elements: ");
+  int lenght = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Input a min possible value: ");
+  int min = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Input a max possible value: ");
+  int max = Convert.ToInt32(Console.ReadLine());
+  double[] myArray = CreateRandomArray(lenght, min, max);
+  PrintArray(myArray);
+  DiffMinMaxValues(myArray);
+  
