63c60c9edff75884d76b6b2d16b0c173b193f8b4
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-12-24 23:09:31 +0300

    Task 3 Initial

--- HW_7/Program.cs
+++ HW_7/Program.cs
@@ -40,6 +40,72 @@
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+// int[,] CreateRandomArray()
+// {
+//     Console.WriteLine("input a number of rows: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("input a number of colums: ");
+//     int colums = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("input a min possible value: ");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("input a max possible value: ");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
+    
+//     int[,] array = new int[rows, colums];
+
+//     for(int i = 0; i < rows; i++)
+//     {
+//         for(int j = 0; j < colums; j++)
+//         {
+//             array[i,j] = new Random().Next(minValue, maxValue);
+//         }
+//     }
+//     return array;
+// }
+// void Show2dArray(int[,] array)
+// {   
+//     Console.WriteLine("----------------------");
+
+//     for(int i = 0; i < array.GetLength(0);i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {   
+//             Console.Write(array[i,j] + "\t");
+//         }
+       
+//        Console.WriteLine(); 
+
+//     }
+
+//     Console.WriteLine("----------------------");
+// }
+
+
+// void FindElementPosition(int[,] array, int row, int column)
+// {
+//     if (array.GetLength(0) > row - 1 && array.GetLength(1) > column - 1)
+//     {
+//         Console.Write($"The value of the element at the selected position: {array[row - 1,column - 1]}");
+//     }
+//     else
+//     {
+//         Console.Write("The indexes entered are not in the array!!!");
+//     }           
+// }
+
+// int[,] myArray = CreateRandomArray();
+// Show2dArray(myArray);
+
+// Console.WriteLine("input a index position of row: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input a index position of column: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// FindElementPosition(myArray, row, column);
+
+
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
 int[,] CreateRandomArray()
 {
     Console.WriteLine("input a number of rows: ");
@@ -74,37 +140,18 @@ void Show2dArray(int[,] array)
         }
        
        Console.WriteLine(); 
-       
+
     }
 
     Console.WriteLine("----------------------");
 }
 
 
-void FindElementPosition(int[,] array, int row, int column)
-{
-    if (array.GetLength(0) > row - 1 && array.GetLength(1) > column - 1)
-    {
-        Console.Write($"The value of the element at the selected position: {array[row - 1,column - 1]}");
-    }
-    else
-    {
-        Console.Write("The indexes entered are not in the array!!!");
-    }           
-}
+
 
 int[,] myArray = CreateRandomArray();
 Show2dArray(myArray);
 
-Console.WriteLine("input a index position of row: ");
-int row = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input a index position of column: ");
-int column = Convert.ToInt32(Console.ReadLine());
-FindElementPosition(myArray, row, column);
-
-
-
-//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 
 
