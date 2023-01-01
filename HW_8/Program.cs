780da01669f37347f958510b5dfec742ae958abd
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2023-01-01 16:30:23 +0300

    Add Task 1 HW 8

--- HW_8/Program.cs
+++ HW_8/Program.cs
@@ -15,6 +15,75 @@
 8 4 4 2 
 */
 
+int[,] CreateRandomArray()
+{
+    Console.WriteLine("input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("input a number of colums: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    
+    int[,] array = new int[rows, colums];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colums; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+void Show2dArray(int[,] array)
+{   
+    Console.WriteLine("--------------------------");
+
+    for(int i = 0; i < array.GetLength(0);i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {   
+            Console.Write(array[i,j] + "\t");
+        }
+       
+       Console.WriteLine(); 
+
+    }
+    
+    Console.WriteLine("--------------------------");
+    
+}
+
+
+void SortArrayRow(int[,] array)
+{    
+   
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+         for (int k = 0; k < array.GetLength(1) - 1; k++)
+         {      
+            if (array[i, k] < array[i, k + 1])
+            {
+                 int tmp = array[i, k + 1];
+                 array[i, k + 1] = array[i, k];
+                 array[i, k] = tmp;
+            }     
+
+         }
+      }
+   }
+
+}
+
+int[,] myArray = CreateRandomArray();
+Show2dArray(myArray);
+SortArrayRow(myArray);
+Show2dArray(myArray);
+
 
 
 /* 
@@ -82,3 +151,7 @@
 11 16 15 06
 10 09 08 07
 */
+
+
+
+
